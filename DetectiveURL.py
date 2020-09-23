5bd05d5611587657f3fa3310bc4dd7db9c021d04
MrEmpy <71796063+MrEmpy@users.noreply.github.com>
2020-09-23 19:45:44 -0300

    Add files via upload

---
+++ DetectiveURL.py
@@ -0,0 +1,44 @@
+import requests
+from colorama import Fore
+
+print(Fore.GREEN + '''
+▒█▀▀▄ █▀▀ ▀▀█▀▀ █▀▀ █▀▀ ▀▀█▀▀ ░▀░ ▀█░█▀ █▀▀ 　 ▒█░▒█ ▒█▀▀█ ▒█░░░ 
+▒█░▒█ █▀▀ ░░█░░ █▀▀ █░░ ░░█░░ ▀█▀ ░█▄█░ █▀▀ 　 ▒█░▒█ ▒█▄▄▀ ▒█░░░ 
+▒█▄▄▀ ▀▀▀ ░░▀░░ ▀▀▀ ▀▀▀ ░░▀░░ ▀▀▀ ░░▀░░ ▀▀▀ 　 ░▀▄▄▀ ▒█░▒█ ▒█▄▄█
+
+              ●❯────────────｢⊙｣───────────❮●
+              ● Tool created by Mr Empy   ●
+              ● Version 1.0               ●
+              ●❯────────────｢⊙｣───────────❮●
+                       ''')
+
+site = input(Fore.GREEN + '[!] Paste the Site URL: ')
+pass_list = input(Fore.GREEN + '[!] Paste your Wordlist directory: ')
+
+print(Fore.GREEN + '')
+print('=================================================')
+print('[+] URL:', site)
+print('[+] Wordlist:', pass_list)
+print('[+] Status: 200, 301, 302, 307, 403')
+print('=================================================')
+print('')
+
+wrd_lst=open(pass_list, "r")
+read_lines=wrd_lst.readlines()
+for line in read_lines:
+    lst=line.strip('\r\n')
+    result=requests.get(site + lst)
+    if (result.status_code == 200):
+        print(Fore.GREEN + '[+]', Fore.WHITE + site + lst, Fore.GREEN + '(status: 200)')
+    if (result.status_code == 204):
+        print(Fore.GREEN + '[+]', Fore.WHITE + site + lst, Fore.GREEN + '(status: 204)')
+    if (result.status_code == 301):
+        print(Fore.GREEN + '[+]', Fore.WHITE + site + lst, Fore.GREEN + '(status: 301)')
+    if (result.status_code == 302):
+        print(Fore.GREEN + '[+]', Fore.WHITE + site + lst, Fore.GREEN + '(status: 302)')
+    if (result.status_code == 307):
+        print(Fore.GREEN + '[+]', Fore.WHITE + site + lst, Fore.GREEN + '(status: 307)')
+    if (result.status_code == 403):
+        print(Fore.GREEN + '[+]', Fore.WHITE + site + lst, Fore.GREEN + '(status: 403)')
+print('')
+print(Fore.GREEN + "[+] Finished!")
